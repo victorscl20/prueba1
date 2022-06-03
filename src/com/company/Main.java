95e53ecc58f72ea7022f3aea6b28df4f2fd2e013
Victor Calderon <victor.calderon@pcup.edu.pe>
2022-06-03 01:35:14 -0500

    menu agregado

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
+import java.awt.*;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// write your code
+        Menu menu  = new Menu();
+
     }
 }
---
+++ src/com/company/menu.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class menu {
+
+    public void menuCreacion(){
+
+    }
+}
+
